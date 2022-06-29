efc21bace7fbc9d565e064f22b64858e3796ff3d
shichiye <shichiye@126.com>
2022-06-29 13:06:59 +0800

    chore: init

---
+++ austin-web/src/main/java/com/scy/austin/AustinApplication.java
@@ -0,0 +1,14 @@
+package com.scy.austin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shichiye
+ */
+@SpringBootApplication
+public class AustinApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AustinApplication.class, args);
+    }
+}
